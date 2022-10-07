88e1b766e9357b0eb2bceddcbdc514cb933ecf2b
gpinot <gpinot@casyope.fr>
2022-10-07 17:00:48 +0200

    add some basic feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import GlobalRefs from '../context/GlobalRefs'
+import ScrollContext from '../context/ScrollContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+  return <GlobalRefs>
+    <ScrollContext>
+      <Component {...pageProps} />
+    </ScrollContext>
+  </GlobalRefs> 
+
 }
 
 export default MyApp
