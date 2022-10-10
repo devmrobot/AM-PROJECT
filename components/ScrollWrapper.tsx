0d525b853294f1b9f98e13b906b5f8923d07c84e
guiguididi <guiguididi@imac-de-guiguididi-1.home>
2022-10-10 22:02:12 +0200

    add some features

--- components/ScrollWrapper.tsx
+++ components/ScrollWrapper.tsx
@@ -1,43 +1,51 @@
-import React, { useContext, useEffect, useRef } from 'react'
-import gsap from 'gsap';
-import ScrollSmoother from 'gsap/ScrollSmoother';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import { globalRefsContext } from '../context/GlobalRefs';
-import { scrollContext } from '../context/ScrollContext';
+import React, { useContext, useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollSmoother from "gsap/ScrollSmoother";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { globalRefsContext } from "../context/GlobalRefs";
+import { scrollContext } from "../context/ScrollContext";
 
+export default function ScrollWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const Globalrefs = useContext(globalRefsContext);
+  const ScrollContext = useContext(scrollContext);
 
-export default function ScrollWrapper({children}:{children:React.ReactNode}) {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-    const Globalrefs = useContext(globalRefsContext);
-    const ScrollContext = useContext(scrollContext);
+    smoother.current = ScrollSmoother.create({
+      wrapper: scrollwrapper.current,
+      content: scrollcontent.current,
+      smooth: 1,
+      effects: true,
+      normalizeScroll: true,
+    });
 
-    useEffect(()=>{
-
-        gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
-
-        smoother.current = ScrollSmoother.create({
-            wrapper:scrollwrapper.current,
-            content:scrollcontent.current,
-            smooth: 1,
-            effects: true,
-            normalizeScroll:true,
-          });
-
-          return ()=>smoother.current.kill();
+    return () => smoother.current.kill();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);  
+  }, []);
 
-    if(!Globalrefs) return null;
-    const {scrollwrapper,scrollcontent} = Globalrefs;
+  if (!Globalrefs) return null;
+  const { scrollwrapper, scrollcontent } = Globalrefs;
 
-    if(!ScrollContext) return null;
-    const { smoother } = ScrollContext;
+  if (!ScrollContext) return null;
+  const { smoother } = ScrollContext;
 
-    return (
-    <div id="smooth-wrapper" ref={scrollwrapper as React.MutableRefObject<HTMLDivElement>} className='absolute'>
-        <div id="smooth-content" ref={scrollcontent as React.MutableRefObject<HTMLDivElement>}>
-            {children}
-        </div>
+  return (
+    <div
+      id="smooth-wrapper"
+      ref={scrollwrapper as React.MutableRefObject<HTMLDivElement>}
+      className="absolute"
+    >
+      <div
+        id="smooth-content"
+        ref={scrollcontent as React.MutableRefObject<HTMLDivElement>}
+      >
+        {children}
+      </div>
     </div>
-  )
+  );
 }
