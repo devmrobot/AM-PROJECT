7fda9d27e42be642fdde9e88b618d053fd0c7cbc
guiguididi <guiguididi@imac-de-guiguididi-1.home>
2022-10-17 22:40:15 +0200

    test gsap

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import Logo from "./Logo";
-import { gsap } from "gsap/dist/gsap";
+import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { globalRefsContext } from "../context/GlobalRefs";
 import Link from "next/link";
